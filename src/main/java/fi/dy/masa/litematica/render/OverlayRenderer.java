9c2b67fa0b0328dc580e889f1dbb0e10132d5668
mms0316 <79147443+mms0316@users.noreply.github.com>
2025-02-23 18:00:38 -0300

    Change Block Info Lines to show items to refill/restock even when not ray tracing to any block. - Also moves these info to the top

--- src/main/java/fi/dy/masa/litematica/render/OverlayRenderer.java
+++ src/main/java/fi/dy/masa/litematica/render/OverlayRenderer.java
@@ -527,16 +527,16 @@ public class OverlayRenderer
                 traceWrapper = RayTraceUtils.getGenericTrace(mc.world, entity, 10, true, targetFluids, false);
             }
 
+            profiler.swap("render_block_lines");
+            if (renderBlockInfoLines)
+            {
+                this.renderBlockInfoLines(traceWrapper, mc, drawContext);
+            }
+
             if (traceWrapper != null &&
                 (traceWrapper.getHitType() == RayTraceWrapper.HitType.VANILLA_BLOCK ||
                  traceWrapper.getHitType() == RayTraceWrapper.HitType.SCHEMATIC_BLOCK))
             {
-                profiler.swap("render_block_lines");
-                if (renderBlockInfoLines)
-                {
-                    this.renderBlockInfoLines(traceWrapper, mc, drawContext);
-                }
-
                 profiler.swap("render_block_overlay");
                 if (renderBlockInfoOverlay)
                 {
@@ -721,53 +721,66 @@ public class OverlayRenderer
     {
         this.blockInfoLines.clear();
 
-        BlockPos pos = traceWrapper.getBlockHitResult().getBlockPos();
-        BlockState stateClient = mc.world.getBlockState(pos);
-        BlockState voidAir = Blocks.VOID_AIR.getDefaultState();
-
-        World worldSchematic = SchematicWorldHandler.getSchematicWorld();
-        BlockState stateSchematic = worldSchematic.getBlockState(pos);
         String ul = GuiBase.TXT_UNDERLINE;
-
         boolean addSep = false;
-        if (stateSchematic != stateClient && stateClient.isAir() == false && stateSchematic.isAir() == false && stateSchematic != voidAir)
-        {
-            this.blockInfoLines.add(ul + "Schematic:");
-            this.addBlockInfoLines(stateSchematic);
-
-            this.blockInfoLines.add("");
-            this.blockInfoLines.add(ul + "Client:");
-            this.addBlockInfoLines(stateClient);
-
-            addSep = true;
-        }
-        else if (traceWrapper.getHitType() == RayTraceWrapper.HitType.SCHEMATIC_BLOCK)
-        {
-            this.blockInfoLines.add(ul + "Schematic:");
-            this.addBlockInfoLines(stateSchematic);
-
-            addSep = true;
-        }
 
         final var ranOutItems = AddonUtils.getRanOutItems();
         final var refillItems = AddonUtils.getRefillItems();
         if (!ranOutItems.isEmpty() || !refillItems.isEmpty()) {
-            if (addSep)
-                this.blockInfoLines.add("");
-
             if (!ranOutItems.isEmpty()) {
                 this.blockInfoLines.add(ul + "Item to restock:");
                 for (var itemStack : ranOutItems) {
                     this.blockInfoLines.add(Registries.ITEM.getId(itemStack.getItem()).toString());
                 }
+
+                addSep = true;
             }
+
             if (!refillItems.isEmpty()) {
+                if (addSep)
+                    this.blockInfoLines.add("");
+
                 this.blockInfoLines.add(ul + "Item to refill:");
                 for (var itemStack : refillItems) {
                     this.blockInfoLines.add(Registries.ITEM.getId(itemStack.getItem()).toString());
                 }
+
+                addSep = true;
             }
-      }
+        }
+
+        if (traceWrapper != null &&
+            (traceWrapper.getHitType() == RayTraceWrapper.HitType.VANILLA_BLOCK ||
+            traceWrapper.getHitType() == RayTraceWrapper.HitType.SCHEMATIC_BLOCK))
+        {
+            BlockPos pos = traceWrapper.getBlockHitResult().getBlockPos();
+            BlockState stateClient = mc.world.getBlockState(pos);
+            BlockState voidAir = Blocks.VOID_AIR.getDefaultState();
+
+            World worldSchematic = SchematicWorldHandler.getSchematicWorld();
+            BlockState stateSchematic = worldSchematic.getBlockState(pos);
+
+            if (stateSchematic != stateClient && stateClient.isAir() == false && stateSchematic.isAir() == false && stateSchematic != voidAir)
+            {
+                if (addSep)
+                    this.blockInfoLines.add("");
+
+                this.blockInfoLines.add(ul + "Schematic:");
+                this.addBlockInfoLines(stateSchematic);
+
+                this.blockInfoLines.add("");
+                this.blockInfoLines.add(ul + "Client:");
+                this.addBlockInfoLines(stateClient);
+            }
+            else if (traceWrapper.getHitType() == RayTraceWrapper.HitType.SCHEMATIC_BLOCK)
+            {
+                if (addSep)
+                    this.blockInfoLines.add("");
+                
+                this.blockInfoLines.add(ul + "Schematic:");
+                this.addBlockInfoLines(stateSchematic);
+            }
+        }
     }
 
     private void addBlockInfoLines(BlockState state)
